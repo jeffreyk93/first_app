6e7f50cd80bb72ffcba6d3804bc4ef975bfdb58e
root <root@raspberrypi.(none)>
2013-12-16 13:01:22 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '0d557d2f7615c68b7e81f48c82a86b1eef70744d0d2e25791c1eb4fa9605cf6e82b859f7827d9efe9ce7db0c5e5e528c58ad494bf7a1ad190808bcce43a6871f'
